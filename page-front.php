b5736489b1f8641b3506a7668018004a02512807
Damian Gover <c3109954@uon.edu.au>
2018-04-04 19:08:15 +1000

    Add Structure to Call Child Pages for Front Page Concept
    The Front Page is a Parent page for navigating the main themes of Emi's work. Work themes will be Children of the declared Front Page.
    
    This is a listing of the Child headings.

---
+++ page-front.php
@@ -0,0 +1,25 @@
+<?php
+/* Template Name: Front Page Template */
+get_header();
+?>
+
+<main>
+    <?php
+        $args = array(
+            'parent' => $post->ID,
+            'post_type' => 'page',
+            'post_status' => 'publish',
+        );
+
+        $pages = get_pages($args);
+    ?>
+
+    <?php foreach ($pages as $page) { ?>
+        <a href="<?php echo get_permalink($page->ID); ?>" rel="bookmark" title="<?php echo $page->post_title; ?>">
+        <header><h1><?php echo $page->post_title; ?></h1></header>
+        <?php }?>
+</main>
+
+<?php
+get_footer();
+?>
