00b99fae7b0a3cbba5cdb8c470c71f47fe1c3fda
Damian Gover <c3109954@uon.edu.au>
2018-04-04 20:36:13 +1000

    Add Mixins for Building Components
    This is going to balloon out. Stick the course until things get _really_ hairy.

--- page-front.php
+++ page-front.php
@@ -15,10 +15,11 @@ get_header();
     ?>
 
     <?php foreach ($pages as $page) { ?>
-        <div class="tile">
+        <div class="front--tile">
             <header>
-                <a href="<?php echo get_permalink($page->ID); ?>" rel="bookmark" title="<?php echo $page->post_title; ?>">
-                <h1><?php echo $page->post_title; ?></h1></a>
+                <a href="<?php echo get_permalink($page->ID); ?>" class="front--tile-link" rel="bookmark" title="<?php echo $page->post_title; ?>">
+                    <h1><?php echo $page->post_title; ?></h1>
+                </a>
             </header>
         </div>
     <?php } ?>
