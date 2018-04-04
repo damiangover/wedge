185eaffc9a8924a2968a9ffa4bc78f70826f3caa
Damian Gover <c3109954@uon.edu.au>
2018-04-04 22:04:53 +1000

    Add Hidden Excerpts on Front Tiles

--- page-front.php
+++ page-front.php
@@ -17,10 +17,13 @@ get_header();
     <?php foreach ($pages as $page) { ?>
         <div class="front--tile">
             <header>
-                <a href="<?php echo get_permalink($page->ID); ?>" class="front--tile-link" rel="bookmark" title="<?php echo $page->post_title; ?>">
+                <a href="<?php echo get_permalink($page->ID); ?>" rel="bookmark" title="<?php echo $page->post_title; ?>">
                     <h1><?php echo $page->post_title; ?></h1>
                 </a>
             </header>
+            <section class="front--tile-excerpt">
+                <?php echo $page->post_excerpt; ?>
+            </section>
         </div>
     <?php } ?>
 </main>
