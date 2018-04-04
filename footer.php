8c53ab5c99d7ecb1d442ffc0e8f63b1b703dd3af
Damian Gover <c3109954@uon.edu.au>
2018-04-05 02:02:15 +1000

    Cull Bloat from Footer

--- footer.php
+++ footer.php
@@ -1,10 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-<script type="text/javascript" src="//cdn.rawgit.com/icons8/bower-webicon/v0.10.7/jquery-webicon.min.js"></script>
-<script src="js/main.js"></script>
-
 <?php wp_footer()?>
-
 </body>
 </html>
