7a8550f46155924d70e45b11aa7edfb7bc495f2f
Damian Gover <c3109954@uon.edu.au>
2018-04-06 00:47:45 +1000

    Add Orientation Styling and Interactivity
    This update handles placing content correctly in column and row modes. Adds accent and transitions for both orientations to give feedback to the user.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 (function($) {
-    window.addEventListener("beforeunload", function () {
-        $("main").addClass("animate-out");
-    });
+    var isHoverEnabled = true;
+
+    function getViewportWidth () {
+        return $(window).width() / parseFloat($("body").css("font-size"));
+    }
 
     $(document).ready(function () {
+
         var focusTile = function () {
             $(this).children(".front--tile-excerpt").toggleClass("show");
             $(this).children(".front--tile-header").toggleClass("move")
             $(this).find(".front--tile-header > span").toggleClass("light");
             $(this).toggleClass("white");
+        }
+
+        var pinkBorderToParent = function() {
+            var n = $(".white").length;
+
+            if (n > 0) {
+                $(this).parent().removeClass("lift-colour");
+                $(this).not($(".white")).parent().addClass("lift-colour");
+            } else {
+                $(this).parent().addClass("lift-colour");
+            }
         }
 
         window.addEventListener("beforeunload", function () {
@@ -20,7 +34,10 @@
         }, 5000);
 
         $('#post').addClass('loading');
+
         $(".front--tile").on("click", focusTile);
+        $(".front--tile").on("click", pinkBorderToParent);        
+        $(".front--tile").on("mouseenter", pinkBorderToParent);
     });
 
 }(jQuery, window));
