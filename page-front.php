fc999a43564cd9215a52eb9f813f05188abdee5c
Damian Gover <c3109954@uon.edu.au>
2018-04-06 00:44:33 +1000

    Flex Small and Normal Text for Vertical Alignment

--- page-front.php
+++ page-front.php
@@ -33,24 +33,22 @@ get_header();
             <header class="front--tile-header">
                 <span class="light"><?php echo $page->post_title; ?>
             </header>
-
             <section class="front--tile-excerpt">
-                <p><?php echo $page->post_excerpt; ?></p>
+                <div class="front--tile-blurb"><?php echo $page->post_excerpt; ?></div>
                 <?php 
                     $count = 1;
                     foreach ($pages_second_level as $sub_page) { 
                 ?>
-                    <nav>
-                        <a href="<?php echo get_permalink($sub_page->ID); ?>" rel="bookmark" title="<?php echo $sub_page->post_title; ?>">
-                            <p>
-                                <small><?php echo '0'.$count; $count++ ?></small>
-                                <?php echo $sub_page->post_title; ?>
-                            </p>
-                        </a>
-                    </nav>
+                <nav>
+                    <a href="<?php echo get_permalink($sub_page->ID); ?>" rel="bookmark" title="<?php echo $sub_page->post_title; ?>">
+                        <div class="line-up-text">
+                            <p><small><?php echo '0'.$count; $count++ ?></small><p>
+                            <p><?php echo $sub_page->post_title; ?></p>
+                        </div>
+                    </a>
+                </nav>
                 <?php } ?>
             </section>
-            
         </div>
     <?php } ?>
 </main>
